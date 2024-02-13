8f2029b4f8e8ad916579865dac290672ff00d8fa
Mitamura Takuma <83048191+mitas30@users.noreply.github.com>
2024-02-13 18:38:26 +0900

    Delete src/test.py

--- src/test.py
+++
@@ -1,8 +0,0 @@
-from services.Gemini.g_func1 import FalseFacer,IdeaBox,BruteThinker
-from threading import Thread
-problem="An unprecedented experience"
-
-bt=BruteThinker(problem=problem)
-thread3=Thread(target=bt.bruteThink)
-thread3.start()
-thread3.join()
